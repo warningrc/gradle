11f51f2eff4247cf127dbb80fd5078b5c86ddcc7
Stefan Wolf <wolf@gradle.com>
2017-12-13 15:33:51 +0100

    Don't re-evaluate task dependencies
    Signed-off-by: Stefan Wolf <wolf@gradle.com>

--- subprojects/core/src/main/java/org/gradle/execution/taskgraph/DefaultTaskExecutionPlan.java
+++ subprojects/core/src/main/java/org/gradle/execution/taskgraph/DefaultTaskExecutionPlan.java
@@ -143,7 +143,7 @@ public class DefaultTaskExecutionPlan implements TaskExecutionPlan {
         }
 
         Set<TaskInfo> visiting = new HashSet<TaskInfo>();
-        CachingTaskDependencyResolveContext context = new CachingTaskDependencyResolveContext();
+        CachingTaskDependencyResolveContext context = new CachingTaskDependencyResolveContext(nodeFactory);
 
         while (!queue.isEmpty()) {
             TaskInfo node = queue.get(0);
