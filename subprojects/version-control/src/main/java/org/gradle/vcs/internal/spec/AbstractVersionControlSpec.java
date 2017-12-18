393869920bece0451d9271f9a0917047bd42bf4a
Sterling Greene <sterling@gradle.com>
2017-12-18 15:29:57 -0500

    Add plugins block to VersionControlSpec

--- subprojects/version-control/src/main/java/org/gradle/vcs/internal/spec/AbstractVersionControlSpec.java
+++ subprojects/version-control/src/main/java/org/gradle/vcs/internal/spec/AbstractVersionControlSpec.java
@@ -17,10 +17,25 @@
 package org.gradle.vcs.internal.spec;
 
 import com.google.common.base.Preconditions;
+import org.gradle.api.Action;
+import org.gradle.api.Transformer;
+import org.gradle.plugin.management.internal.DefaultPluginRequest;
+import org.gradle.plugin.management.internal.DefaultPluginRequests;
+import org.gradle.plugin.management.internal.PluginRequestInternal;
+import org.gradle.plugin.management.internal.PluginRequests;
+import org.gradle.plugin.use.PluginDependenciesSpec;
+import org.gradle.plugin.use.PluginDependencySpec;
 import org.gradle.vcs.VersionControlSpec;
+
+import javax.annotation.Nullable;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.gradle.util.CollectionUtils.collect;
 
 public abstract class AbstractVersionControlSpec implements VersionControlSpec {
     private String rootDir = "";
+    private final CollectingPluginDependenciesSpec plugins = new CollectingPluginDependenciesSpec();
 
     @Override
     public String getRootDir() {
@@ -31,5 +46,59 @@ public abstract class AbstractVersionControlSpec implements VersionControlSpec {
     public void setRootDir(String rootDir) {
         Preconditions.checkNotNull(rootDir, "rootDir should be non-null for '%s'.", getDisplayName());
         this.rootDir = rootDir;
+    }
+
+    @Override
+    public void plugins(Action<? super PluginDependenciesSpec> configuration) {
+        configuration.execute(plugins);
+    }
+
+    PluginRequests getPluginRequests() {
+        if (plugins.specs.isEmpty()) {
+            return DefaultPluginRequests.EMPTY;
+        } else {
+            List<PluginRequestInternal> pluginRequests = collect(plugins.specs, new Transformer<PluginRequestInternal, RequestedPluginDependency>() {
+                public PluginRequestInternal transform(RequestedPluginDependency original) {
+                    // TODO:
+                    return new DefaultPluginRequest(original.id, original.version, original.apply, null, "included build");
+                }
+            });
+
+            return new DefaultPluginRequests(pluginRequests);
+        }
+    }
+
+    private static class RequestedPluginDependency implements PluginDependencySpec {
+        private final String id;
+        private String version;
+        private boolean apply;
+
+        private RequestedPluginDependency(String id) {
+            this.id = id;
+            this.apply = true;
+        }
+
+        @Override
+        public PluginDependencySpec version(@Nullable String version) {
+            this.version = version;
+            return this;
+        }
+
+        @Override
+        public PluginDependencySpec apply(boolean apply) {
+            this.apply = apply;
+            return this;
+        }
+    }
+
+    private static class CollectingPluginDependenciesSpec implements PluginDependenciesSpec {
+        private final List<RequestedPluginDependency> specs = new LinkedList<RequestedPluginDependency>();
+
+        @Override
+        public PluginDependencySpec id(String id) {
+            RequestedPluginDependency spec = new RequestedPluginDependency(id);
+            specs.add(spec);
+            return spec;
+        }
     }
 }
