97c722be8bf47b6df7ece1ce1f1a8bf6a9d8fb21
Paul Merlin <paul@gradle.com>
2017-12-20 13:04:18 +0100

    Extract idiomatic strict-compile plugin to buildSrc
    Sub projects request the plugin if needed.
    
    The plugin configures the Java and Groovy compilation tasks according
    to the `strictCompile` extension configuration.
    
    No more Groovy Closures as a Project extra properties.
    
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -36,6 +36,10 @@ gradlePlugin {
             id = "test-fixtures"
             implementationClass = "org.gradle.plugins.testfixtures.TestFixturesPlugin"
         }
+        "strictCompile" {
+            id = "strict-compile"
+            implementationClass = "org.gradle.plugins.strictcompile.StrictCompilePlugin"
+        }
         "jsoup" {
             id = "jsoup"
             implementationClass = "org.gradle.plugins.jsoup.JsoupPlugin"
